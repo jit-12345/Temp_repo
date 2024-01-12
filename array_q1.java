f2d11f5c969b39a3031d6da987f5720f298d681b
jit-12345 <jitchak95@gmail.com>
2024-01-12 15:19:55 +0530

    array_q1.java is modified

--- array_q1.java
+++ array_q1.java
@@ -7,7 +7,7 @@ public class array_q1 {
     }
     
     public static void main(String[] args) {
-        int nums[]={1,2,3,1};
+        int nums[]={1,2,3,5};
         System.out.println( checkdistinct(nums));
         
     }
