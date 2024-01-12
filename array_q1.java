5a64db173df4375909d77a6c4698c86aa8f894ca
jit-12345 <jitchak95@gmail.com>
2024-01-12 11:29:18 +0530

    array_q1.java a function is totally modefied

--- array_q1.java
+++ array_q1.java
@@ -1,13 +1,6 @@
 public class array_q1 {
-    public static boolean checkdistinct(int nums[]){
-        for (int i = 0; i < nums.length-1; i++) {
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    return true;
-                }
-            }
-        }
-        return false;
+    public static int dugdugi(){
+        return 0;
     }
     
     public static void main(String[] args) {
