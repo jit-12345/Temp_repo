cc43fcbe172a5c69aa8dcaf4a679d3f0e8931773
Jit Chakraborty <jitchak95@gmail.com>
2024-01-12 15:21:09 +0530

    the array is changed on remote
    remote changes

--- array_q1.java
+++ array_q1.java
@@ -14,7 +14,7 @@ public class array_q1 {
     }
     
     public static void main(String[] args) {
-        int nums[]={1,2,3,1};
+        int nums[]={1,2,3,9};
         System.out.println( checkdistinct(nums));
         
     }
